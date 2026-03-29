c26cd53f9d4ef3cd784dc1c4e85605bc8fffdc5d
romankarski.1 <karski.roman@gmail.com>
2026-03-29 19:41:44 +0200

    Update marky 0.1.2 fix

--- Formula/marky.rb
+++ Formula/marky.rb
@@ -2,7 +2,7 @@ class Marky < Formula
   desc "Local-first markdown workspace for browsing and editing notes"
   homepage "https://github.com/romankarski/Marky"
   version "0.1.1"
-  url "https://github.com/romankarski/Marky/releases/download/v0.1.1/marky-0.1.1.tgz"
+  url "https://github.com/romankarski/Marky/releases/download/v0.1.2/marky-0.1.2.tgz"
   sha256 "800dd919b237c2cd038875dc55f11838a1dcc394c478d379d14f05bfc761df33"
   license "MIT"
 
